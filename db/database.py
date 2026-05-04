042ee15723bfd3a34078613fe7c383e4cf50eb0d
rockbottle <mpramod.mkd@gmail.com>
2026-05-04 13:52:01 +0400

    v1

--- db/database.py
+++ db/database.py
@@ -4,10 +4,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # 1. Update the Connection URL
-# Format: postgresql://user:password@host:port/database
-# 'db' is the service name if using Docker/Podman networks
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL","postgresql://pguser:P%40ssw0rd@dcim-db:5432/dcim")
-
+#SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL","postgresql://pguser:P%40ssw0rd@dcim-db:5432/dcim")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 # 2. Create the Engine
 # We remove connect_args={"check_same_thread": False} as it is SQLite-only
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
